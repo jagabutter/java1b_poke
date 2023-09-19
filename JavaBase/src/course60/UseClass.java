c60f3fc7243a786ef1054ca263872d83dc1ed6ec
S25 <S25@MSI>
2023-09-19 10:32:33 +0900

    ６０回java授業 ポリモーフィズム オーバーライド #2

---
+++ JavaBase/src/course60/UseClass.java
@@ -0,0 +1,16 @@
+package course60;
+
+public class UseClass {
+	public static void main(String[] args) {
+		Animal dog = new Dog();
+		Animal cat = new Cat();
+		Animal animal =new Animal();
+		
+
+		//ぽりもーふぃずむ
+		dog.nakigoe();
+		cat.nakigoe();
+		animal.nakigoe();
+
+	}
+}
